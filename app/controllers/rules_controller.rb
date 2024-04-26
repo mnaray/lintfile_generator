1a0eacc00526a583040f7692ae20b65624f6fe5f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-04-26 14:42:35 +0200

    add controller logic to process form data

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,4 +1,31 @@
 class RulesController < ApplicationController
   def index
   end
+
+  def result
+    newline_string = "\r\n        "
+    rules_string = ""
+
+    if params[:accessor_pairs].present?
+      rules_string = rules_string + "\"accessor-pairs\": \"error\"," + newline_string
+    end
+
+    if params[:arrow_body_style].present?
+      rules_string = rules_string + "\"arrow-body-style\": [\"error\", \"always\"]," + newline_string
+    end
+
+    if params[:block_scoped_var].present?
+      rules_string = rules_string + "\"block-scoped-var\": \"error\"," + newline_string
+    end
+
+    if  params[:camelcase].present?
+      rules_string = rules_string + "\"camelcase\": \"error\"," + newline_string
+    end
+
+    if  params[:capitalized_comments].present?
+      rules_string = rules_string + "\"capitalized-comments\": \"error\"," + newline_string
+    end
+
+    @result = rules_string.strip
+  end
 end
